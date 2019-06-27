3bfa8d34bf076b1917a523b7414dd0000f544854
manya0393 <50484575+manya0393@users.noreply.github.com>
2019-06-27 13:21:16 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+    <p> <strong> <center> Welcome Aakansha :) </center> </strong> </p>
+    <?php
+echo "Hello World!";
+?>
+    
+        <table border="2">
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+        </tr>
+        <tr>
+          <td><button type="button">Jill Smith</button></td>
+          <td>50</td>
+        </tr>
+        <tr>
+          <td><button type="button">Eve Jackson</button></td>
+          <td>94</td>
+        </tr>
+      </table>
+    </div>
+
+</html>
